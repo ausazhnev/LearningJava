cf40dc870f1cae9206e619a53e8c18d6f6f09945
Panda <ausazhnev@gmail.com>
2025-01-09 23:22:09 +0400

    feat: added Dog class with attributes and methods
    - Implemented `Dog` class with attributes: `size`, `breed`, and `name`.
    - Added a `bark` method to simulate a dog's bark by printing "Гав! Гав!" to the console.
    - Serves as a basic example of defining a class in Java with attributes and methods.
    
    ---
    feat: добавлен класс Dog с атрибутами и методами
    
    - Реализован класс `Dog` с атрибутами: `size`, `breed` и `name`.
    - Добавлен метод `bark`, который имитирует лай собаки, выводя "Гав! Гав!" в консоль.
    - Используется как базовый пример определения класса в Java с атрибутами и методами.

---
+++ src/ru/pandatech/headfirstjava/dog/Dog.java
@@ -0,0 +1,11 @@
+package ru.pandatech.headfirstjava.dog;
+
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bakr() {
+        System.out.println("Гав! Гав!");
+    }
+}
