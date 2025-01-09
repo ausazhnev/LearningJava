aa9fbdd5a6524ce1f74290745f2094f741f05ae5
Panda <ausazhnev@gmail.com>
2025-01-10 00:12:48 +0400

    feat: added Movie class with attributes and methods
    - Implemented `Movie` class with attributes: `title`, `genre`, and `rating`.
    - Added a `playIt()` method to simulate playing the movie by printing "Проигрывание фильма" to the console.
    - Serves as a demonstration of creating a Java class with attributes and a simple method.
    
    ---
    feat: добавлен класс Movie с атрибутами и методом
    
    - Реализован класс `Movie` с атрибутами: `title`, `genre` и `rating`.
    - Добавлен метод `playIt()`, который имитирует воспроизведение фильма, выводя "Проигрывание фильма" в консоль.
    - Используется как пример создания класса в Java с атрибутами и простым методом.

---
+++ src/ru/pandatech/headfirstjava/movie/Movie.java
@@ -0,0 +1,11 @@
+package ru.pandatech.headfirstjava.movie;
+
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Проигрывание фильма");
+    }
+}
