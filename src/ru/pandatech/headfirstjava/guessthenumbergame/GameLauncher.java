e7010def5d36d5e9e44fd55f80babad1eb83f3ad
Panda <ausazhnev@gmail.com>
2025-01-11 11:53:00 +0400

    feat: added GameLauncher class as entry point for the guessing game
    - Implemented `GameLauncher` class with the `main()` method to start the application.
    - Created an instance of `GuessGame` and called the `startGame()` method to begin the game.
    
    ---
    feat: добавлен класс GameLauncher — точка входа для игры в угадывание
    
    - Реализован класс `GameLauncher` с методом `main()` для запуска приложения.
    - Создан экземпляр `GuessGame` и вызван метод `startGame()` для начала игры.

---
+++ src/ru/pandatech/headfirstjava/guessthenumbergame/GameLauncher.java
@@ -0,0 +1,8 @@
+package ru.pandatech.headfirstjava.guessthenumbergame;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
