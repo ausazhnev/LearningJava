5d2dbed8760a8cc62094b470d1e91c52209a619e
Panda <ausazhnev@gmail.com>
2025-01-12 15:07:39 +0400

    feat: added PoorDog class to demonstrate default variable values
    - Implemented `PoorDog` class with private attributes `size`, `weight`, `name`, and `isDomestic`.
    - Added getter methods `getSize()`, `getName()`, `getDomestic()`, and `getWeight()` to access the default values of the attributes.
    - The class demonstrates the default values assigned to variables:
      - `int` defaults to `0`.
      - `float` defaults to `0.0`.
      - `String` defaults to `null`.
      - `boolean` defaults to `false`.
    
    ---
    feat: добавлен класс PoorDog для демонстрации значений по умолчанию
    
    - Реализован класс `PoorDog` с приватными атрибутами `size`, `weight`, `name` и `isDomestic`.
    - Добавлены геттеры `getSize()`, `getName()`, `getDomestic()`, `getWeight()` для доступа к значениям атрибутов.
    - Класс демонстрирует значения по умолчанию для переменных:
      - `int` — `0`.
      - `float` — `0.0`.
      - `String` — `null`.
      - `boolean` — `false`.

---
+++ src/ru/pandatech/headfirstjava/poordog/PoorDog.java
@@ -0,0 +1,24 @@
+package ru.pandatech.headfirstjava.poordog;
+
+public class PoorDog {
+    private int size;
+    private float weight;
+    private String name;
+    private boolean isDomestic;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getDomestic() {
+        return isDomestic;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
