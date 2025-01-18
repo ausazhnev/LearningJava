2e6d6824818fd38563de9a1d9e273f5beacdfc95
Panda <ausazhnev@gmail.com>
2025-01-18 20:58:18 +0400

    feat: added SimpleDotComGame class to start and manage the game
    - Implemented `SimpleDotComGame` class with the `main()` method to launch the game.
    - Initialized game components, including:
      - `GameHelper` for handling user input.
      - `SimpleDotCom` for managing the game's logic.
    - Randomly generated the starting location for the "site" within the range 0–6.
    - Managed the game loop:
      - Prompted the user for input using `GameHelper`.
      - Checked the user's guess using `SimpleDotCom`.
      - Kept track of the number of guesses and printed the final score when the "site" was sunk.
      - Allowed users to exit the game early by typing "exit".
    - Printed messages to guide the player and indicate the game's progress.
    
    ---
    feat: добавлен класс SimpleDotComGame для запуска и управления игрой
    
    - Реализован класс `SimpleDotComGame` с методом `main()` для запуска игры.
    - Инициализированы компоненты игры, включая:
      - `GameHelper` для обработки пользовательского ввода.
      - `SimpleDotCom` для управления логикой игры.
    - Случайным образом задано начальное расположение "сайта" в диапазоне 0–6.
    - Организован игровой цикл:
      - Запрашивался ввод пользователя с помощью `GameHelper`.
      - Проверялось предположение игрока с использованием `SimpleDotCom`.
      - Вёлся учёт количества попыток и выводился итоговый результат при потоплении "сайта".
      - Реализована возможность досрочного выхода из игры при вводе "exit".
    - Добавлены сообщения для управления процессом игры и информирования игрока о ходе игры.

---
+++ src/ru/pandatech/headfirstjava/gamedotcom/SimpleDotComGame.java
@@ -0,0 +1,34 @@
+package ru.pandatech.headfirstjava.gamedotcom;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        byte maxNum = 6;
+        GameHelper halper = new GameHelper();
+        SimpleDotCom theDotCom = new SimpleDotCom();
+
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] location = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLocationCell(location);
+
+        System.out.println("Игра началась.");
+        boolean isAlive = true;
+        while (isAlive) {
+            String guess = halper.getUserInput("Введите число от 0 до 6: ", maxNum);
+            if (guess.equalsIgnoreCase("exit")) {
+                System.out.println("Досрочное завершение игры.");
+                break;
+            }
+            String result = theDotCom.checkYourSelf(guess);
+            numOfGuesses++;
+
+            if (result.equalsIgnoreCase("потопил")) {
+                isAlive = false;
+                System.out.println("Вы победили!!!");
+                System.out.printf("Вам потребовалось %d попыток(и)", numOfGuesses);
+            }
+        }
+
+    }
+}
